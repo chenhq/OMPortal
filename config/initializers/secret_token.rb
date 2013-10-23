bc0b4da4785ce68710280aebf82fccffbce0a90c
hongqing.chen <hongqing.chen@gmail.com>
2013-10-23 15:31:45 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OMPortal::Application.config.secret_token = 'd69dd8abbfa5058318e3ada557be4be37b631fc4d8f5aea18ea714347f44c7ff4db138fd8e3e30b26bbe0d8d225006ebfe5ef16205b120b1b6f9ed5d0f09381d'
